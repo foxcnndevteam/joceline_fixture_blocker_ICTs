69c620852ddf95f7f83e8f00215bee7f81e39155
NestorNey <98431315+NestorNey@users.noreply.github.com>
2025-01-04 14:53:22 -0600

    Modified Models class

--- Db/Models.py
+++ Db/Models.py
@@ -1,61 +1,38 @@
 import os
-import json
-import sys
-from env import BASE_DIR
+
 from peewee import *
+from env import BASE_DIR
 
-localDBPath = os.path.join(BASE_DIR, "jocelinefb.db")
-localDB = SqliteDatabase(localDBPath)
+class LocalMetadata:
+    localDBPath = os.path.join(BASE_DIR, "jocelinefb.db")
+    localDB = SqliteDatabase(localDBPath)
 
+class Local:
+    class Fixture(Model):
+        fixture_id = TextField(unique=True)
+        fail_count = IntegerField()
+        online = BooleanField()
 
-class Fixture(Model):
-    fixture_id = TextField(unique=True)
-    fail_count = IntegerField()
-    online = BooleanField()
+        class Meta:
+            database = LocalMetadata.localDB
 
-    class Meta:
-        database = localDB
+    class User(Model):
+        id = AutoField()
+        username = TextField(unique=True)
+        password = TextField()
 
-class User(Model):
-    id = AutoField()
-    username = TextField(unique=True)
-    password = TextField()
+        class Meta:
+            database = LocalMetadata.localDB
 
-    class Meta:
-        database = localDB
+    class Config(Model):
+        configId = IntegerField(unique=True)
+        max_fail_count = IntegerField()
+        block_pass = TextField()
+        externDbPath = TextField()
+        language = TextField()
 
-class Config(Model):
-    configId = IntegerField(unique=True)
-    max_fail_count = IntegerField()
-    block_pass = TextField()
+        class Meta:
+            database = LocalMetadata.localDB
 
-    class Meta:
-        database = localDB
-
-localDB.connect()
-localDB.create_tables([Fixture, User, Config], safe=True)
-
-# ---------------------------- Extern DB ---------------------------- #
-
-confFileName = "jocelinefb.conf.json"
-
-with open(os.path.join(BASE_DIR, confFileName), 'r') as file:
-    confFile = json.loads(file.read())
-globalDBPath = confFile["externDBPath"]
-
-globalDB = SqliteDatabase(globalDBPath)
-
-class TestInfo(Model):
-    id = AutoField()
-    serial = TextField()
-    fail_reason = TextField()
-    fixture_id = TextField()
-
-    class Meta:
-        database = globalDB
-try:
-    globalDB.connect()
-    globalDB.create_tables([TestInfo], safe=True)
-except OperationalError:
-    print("ERROR: No se encontro la ruta a la base de datos.")
-    sys.exit(1)
+    LocalMetadata.localDB.connect()
+    LocalMetadata.localDB.create_tables([Fixture, User, Config], safe=True)
