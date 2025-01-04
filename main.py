ba2a37b4f557bf66ec3bc0f6835273d0954c1be5
NestorNey <98431315+NestorNey@users.noreply.github.com>
2025-01-04 14:53:40 -0600

    Modified imports and app instances

--- main.py
+++ main.py
@@ -1,107 +1,142 @@
+import sys
+import time
 import typer
-import os
-import psutil
 import logger
 import atexit
-from Manager.FixtureManager import FixtureManager
-from Manager.UserManager    import UserManager
-from Manager.ConfigManager  import ConfigManager
-from Views.BlockedWindow import BlockedWindow
+import Utils.lang as lang
+import Manager.user as user
+import Manager.config as config
+import Manager.fixture as fixture
+
+from rich import print
 from Views.RetestWindow  import RetestWindow
+from Views.BlockedWindow import BlockedWindow
+from typing_extensions import Annotated
+
 
 # --- Exit handler --- #
-# Used to save logs when exit
+# Used to save logs when app exit
+
 def exit_handler():
     logger.saveLogs()
+    time.sleep(3)
+    print()
 atexit.register(exit_handler)
 
 
 # --- App start --- #
 
-logger.configureLogger()
+def startAppData():
+    logger.configureLogger()
+
+    config.loadRawConfig()
+    config.loadConfigInDb()
+
+    lang.loadMessages()
+    fixture.loadFixtureInfo()
+
 try:
-    app = typer.Typer( add_completion=False )
+    app = typer.Typer(add_completion=False)
 
-    set    = typer.Typer()
-    get    = typer.Typer()
-    config = typer.Typer()
-    test   = typer.Typer()
-    window = typer.Typer()
+    set_command = typer.Typer()
+    get_command = typer.Typer()
+    test_command = typer.Typer()
+    config_command = typer.Typer()
+    window_command = typer.Typer()
 
-    set.add_typer( config, name="config" )
-    app.add_typer( set   , name="set"    )
-    app.add_typer( test  , name="test"   )
-    app.add_typer( get   , name="get"    )
-    app.add_typer( window, name="window" )
 
-    fm = FixtureManager()
-    um = UserManager()
-    cm = ConfigManager()
+    app.add_typer(set_command, name="set")
+    app.add_typer(get_command, name="get")
+    app.add_typer(test_command, name="test")
+    app.add_typer(window_command, name="window")
+    set_command.add_typer(config_command, name="config")
 
-    @test.command()
-    def saveresult( result: str, serial: str, fixtureid: str, failstatus: int ):
-        fm.onTestSave( result, serial, fixtureid, failstatus )
+    @test_command.command()
+    def saveresult( result: str, serial: str, fixtureid: str, failstatus: int, not_open_retestview: Annotated[bool, typer.Option("--not-open-retestview")] = False ):
+        fixture.onTestSave( result, serial, fixtureid, failstatus, not_open_retestview )
 
     @app.command()
     def createsuperuser( username: str, password: str ):
-        um.createSuperUser( username, password )
+        user.createSuperUser( username, password )
 
     @app.command()
     def savefixtureonline():
-        fm.saveOnlineResultInPath()
+        fixture.saveOnlineResultInPath()
 
-    # --- Set commands --- #
+#     # --- Set commands --- #
 
-    @set.command()
+    @set_command.command()
     def failcount( failcount ):
-        if um.authUser() == "PASS":
-            fm.setFailCount( failcount )
-            print( "The fail count where set" )
+        if user.authUser() == "PASS":
+            fixture.setFailCount( failcount )
+            try:
+                logger.info(lang.messages["setter"]["fail_count"])
+            except KeyError as e:
+                logger.error(f'Corrupted lang file: Missing key "{e.args[0]}" in lang file')
+                sys.exit(0)
 
-    # --- Get commands --- #
+#     # --- Get commands --- #
 
-    @get.command()
+    @get_command.command()
     def failcount():
-        print( "Fails count: " + str( fm.getFailCount() ) )
+        print( "[bold]Fails count:[/bold] " + str( fixture.getFailCount() ) )
 
-    # --- Config Commands --- #
+#     # --- Config Commands --- #
 
-    @config.command()
+    @config_command.command()
     def maxfailcount( maxfailcount: int ):
-        if um.authUser() == "PASS":
-            cm.setMaxFailCount( maxfailcount )
-            print( "The max fail count where set" )
+        if user.authUser() == "PASS":
+            config.setMaxFailCount( maxfailcount )
+            try:
+                logger.info(lang.messages["setter"]["max_fail_count"])
+            except KeyError as e:
+                logger.error(f'Corrupted lang file: Missing key "{e.args[0]}" in lang file')
+                sys.exit(0)
 
-    @config.command()
+    @config_command.command()
     def blockpassword( newpassword: str ):
-        if um.authUser() == "PASS":
-            cm.setBlockPassword( newpassword )
-            print( "The block password where set" )
+        if user.authUser() == "PASS":
+            config.setBlockPassword( newpassword )
+            try:
+                logger.info(lang.messages["setter"]["block_password"])
+            except KeyError as e:
+                logger.error(f'Corrupted lang file: Missing key "{e.args[0]}" in lang file')
+                sys.exit(0)
 
-    # --- window commands --- #
+#     # --- window commands --- #
 
-    @window.command()
-    def open( view: str ):
+    @window_command.command()
+    def open( view: str, show_boards: Annotated[bool, typer.Option("--show-boards")] = False ):
         if view == "blockedView":
+            pass
             blockWindow = BlockedWindow("failsLimitReached")
             blockWindow.open()
         elif view == "retestView":
-            retestWindow = RetestWindow()
+            retestWindow = RetestWindow(show_boards)
             retestWindow.open()
         else:
-            print("La view no existe")
+            try:
+                logger.error(lang.messages["view_not_exist"])
+            except KeyError as e:
+                logger.error(f'Corrupted lang file: Missing key "{e.args[0]}" in lang file')
+                sys.exit(0)
 
-    # --- test debug command ---#
+#     # --- test debug command ---#
 
-    @app.command()
-    def testprogram():
-        ppid = os.getppid()
-        print(psutil.Process(ppid).name())
+#     @app.command()
+#     def testprogram():
+#         ppid = os.getppid()
+#         print(psutil.Process(ppid).name())
 
     if __name__ == "__main__":
+        print()
+        startAppData()
         app()
 
 except Exception as e:
-    print("Ocurrio un error inesperado, contacta al equipo de desarollo para resolver el problema.")
-    logger.error("ERROR PRINCIPAL")
-
+    try:
+        logger.error(lang.messages["unexpected_error"])
+    except KeyError as e:
+        logger.error(f'Corrupted lang file: Missing key "{e.args[0]}" in lang file')
+        logger.error('An unexpected error occurred, please contact support team')
+    logger.crash(f'FATAL_ERROR: {e.args}') 
