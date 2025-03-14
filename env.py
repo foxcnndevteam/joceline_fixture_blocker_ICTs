27f8dabfbcdb1e603fafcad8d329b375c24e39f7
NestorNey <98431315+NestorNey@users.noreply.github.com>
2025-03-14 15:48:46 -0600

    Modified: Static files

--- env.py
+++ env.py
@@ -4,4 +4,4 @@ import sys
 if getattr(sys, 'frozen', False):
     BASE_DIR = os.path.dirname(os.path.abspath(sys.executable))
 elif __file__:
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
