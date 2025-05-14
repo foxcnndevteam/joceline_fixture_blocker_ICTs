1e550cb56e8de0122bf96e37bb01093f4c9f78b9
kintaro335-sp <kintaroponce.uni@gmail.com>
2025-05-13 21:08:47 -0600

    added version to the help

--- utils/help_printer.py
+++ utils/help_printer.py
@@ -1,5 +1,6 @@
 import sys
 from gui.assets import get_asset
+from env import VERSION_PROGRAM
 
 def get_help() -> str:
   app_name = sys.argv[0].replace('./', '')
@@ -8,4 +9,5 @@ def get_help() -> str:
   with open(help_data_dir) as file_help:
     data_help = file_help.read()
     data_help = data_help.replace('${{ process_name }}', app_name)
+    data_help = data_help.replace('${{ version }}', VERSION_PROGRAM)
     print(data_help)
